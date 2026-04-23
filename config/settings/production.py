b80a640052297f08cf9e4510eb93c320ee8b9e74
BoastfulApex <88885919+BoastfulApex@users.noreply.github.com>
2026-04-23 14:45:10 +0500

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,3 +5,9 @@ DEBUG = False
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = "DENY"
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://ijro.sportedu.uz",
+    "https://www.ijro.sportedu.uz",
+    "https://api-ijro.sportedu.uz",
+]
